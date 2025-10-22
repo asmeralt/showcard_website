93881e996d42d1047607570de2277f3af34b4f9f
asmeralt <bohdan.vasylyshyn@pm.me>
2025-10-23 01:49:08 +0300

    Image model added

--- src/backend/showcard_website_api/models/admin.py
+++ src/backend/showcard_website_api/models/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from showcard_website_api.models.example import JigsawExample
+from showcard_website_api.models.example import JigsawExample, JigsawExampleImage
 
 admin.site.register(JigsawExample)
+admin.site.register(JigsawExampleImage)
--- src/backend/showcard_website_api/models/example.py
+++ src/backend/showcard_website_api/models/example.py
@@ -1,4 +1,16 @@
+import uuid
+
 from django.db import models
+
+
+class JigsawExampleImage(models.Model):
+
+    class Meta:
+        db_table = "jigsaw_example_image"
+
+    id = models.UUIDField(name="index", primary_key=True, unique=True, default=uuid.uuid4, editable=False)
+    url = models.URLField(name="url")
+    alt_text = models.CharField(name="alt_text", max_length=140)
 
 
 class JigsawExample(models.Model):
@@ -6,6 +18,7 @@ class JigsawExample(models.Model):
     class Meta:
         db_table = "jigsaw_example"
 
-    id = models.UUIDField(name="index", primary_key=True, unique=True)
+    id = models.UUIDField(name="index", primary_key=True, unique=True, default=uuid.uuid4, editable=False)
     name = models.CharField("name", max_length=30)
     description = models.TextField(max_length=1000)
+    images = models.ManyToManyField(to=JigsawExampleImage)
